1dfd6a52fb5ea9707363896555e4efd1e8ba6d4e
evan <evan@ghtinc.com>
2022-03-14 02:37:41 +0800

    refactor(web_server): clean code

--- web_server/src/index.ts
+++ web_server/src/index.ts
@@ -9,27 +9,27 @@ import rateLimit from "express-rate-limit";
 import handleLog from "./middleware/handleLog";
 import logger from "./utils/logger";
 
+const PORT = process.env.PORT || 4000;
+
 const startupServer = async () => {
   const server = express();
   const limiter = rateLimit({
     windowMs: 1000 * 60 * 10,
     max: 1000,
   });
-  //const __filename = fileURLToPath(import.meta.url);
-  // const __dirname = path.dirname(__filename);
-
+  
   server
     //Removes the X-Powered-By header, which is set by default in some frameworks
     .disable("x-powered-by")
+    // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+    // see https://expressjs.com/en/guide/behind-proxies.html
+    .set("trust proxy", 1)
     .use(
-      express.static(
-        path.join(
-          __dirname,
-          "../../frontend/build"
-        )
-      ),
+      express.static(path.join(__dirname, "../../frontend/build")),
+      express.text(),
       express.json(),
       express.urlencoded({ extended: true }),
+      express.raw(),
       cookieParser(),
       compression(),
       limiter,
@@ -45,27 +45,20 @@ const startupServer = async () => {
       //   }
       // })
     )
-        // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
-    // see https://expressjs.com/en/guide/behind-proxies.html
-    .set("trust proxy", 1);
-  
-  //for health check
-  server.get("/ping", (req, res) => res.send("pong"));
-  
-  server.get("*",(req, res) => {
-    res.render("index");
-  });
-
-  const PORT = process.env.PORT || 4000;
-  server.listen(PORT, () =>
-    logger.info(
-      chalk.red.bgBlue.bold.underline(
-        `Server ready at http://localhost:${PORT}`
+    //for health check
+    .get("/ping", (req, res) => res.send("pong"))
+    .get("*", (req, res) => {
+      res.render("index");
+    })
+    .listen(PORT, () =>
+      logger.info(
+        chalk.red.bgBlue.bold.underline(
+          `Server ready at http://localhost:${PORT}`
+        )
       )
-    )
-  );
+    );
 };
 
 startupServer();
 
-export { startupServer }
+export { startupServer };
